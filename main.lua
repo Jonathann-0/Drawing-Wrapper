5e1c7e9b529604b4c824986bb0b5338e6c490a91
Jonathann-0 <76716757+Jonathann-0@users.noreply.github.com>
2020-12-28 19:30:24 -0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,47 @@
+if not Drawing or Drawing.__CONTAINER or getmetatable(Drawing) ~= nil then 
+    return
+end
+
+Drawing = setmetatable(Drawing, {
+    __index = function(self, i)
+        if i == 'Clear' then
+            return function()
+                for _, shape in pairs(self.__CONTAINER) do
+                    shape:Remove()
+                end
+                if self.Length > 0 then
+                    self:Clear()
+                end
+                return self;
+            end
+        elseif i == 'Length' then
+            return #self.__CONTAINER
+        end
+        return nil
+    end
+})
+
+Drawing.__CONTAINER = {}
+
+local pseudoLine = Drawing.new("Line")
+local metadata = getmetatable(pseudoLine)
+local oldIndex = metadata.__index
+pseudoLine:Remove()
+
+metadata.__index = function(self, i)
+    if i:lower() == 'remove' or i:lower() == 'delete' or i:lower() == 'destroy' then
+        local index = table.find(Drawing.__CONTAINER, self)
+        if index ~= nil then
+            table.remove(Drawing.__CONTAINER, index)
+        end
+        i = 'Remove'
+    end
+    return oldIndex(self, i)
+end
+
+local func;
+func = hookfunction(Drawing.new, function(str)
+    local shape = func(str);
+    table.insert(Drawing.__CONTAINER, shape)
+    return shape
+end)
